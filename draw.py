31c5f7cd124e4657bea51a0832def95c84d46816
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2022-12-24 12:35:21 +0545

    Add files via upload

---
+++ draw.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.bgcolor("black")
+
+
+squary = turtle.turtle()
+squary.speed(20)
+squary.pencolor("red")
+for i in range(400):
+    squary.forward(i)
+    squary.left(91)
+
+
+
+# import turtle as tur
+# import colorsys as cs
+# tur.setup(800,800)
+# tur.speed(0)
+# tur.tracer(10)
+# tur.width(2)
+# tur.bgcolor("black")
+# for j in range(25):
+#     for i in range(15):
+#         tur.color(cs.hsv_to_rgb(i/15,j/25,1))
+#         tur.right(90)
+#         tur.circle(200-j*4,90)
+#         tur.left(90)
+#         tur.circle(200-j*4,90)
+#         tur.right(180)
+#         tur.circle(50,24)
+# tur.hideturtle()
+# tur.done
+
