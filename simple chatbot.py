bc39fe2b057138975d763adb007f7e614798fb04
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-01-22 19:30:29 +0545

    Add files via upload

---
+++ simple chatbot.py
@@ -0,0 +1,29 @@
+import random
+
+# Define a list of predefined responses
+responses = {
+    "hi": ["Hello!", "Hi there!"],
+    "how are you": ["I'm good, thanks for asking.", "I'm doing well, how about you?"],
+    "bye": ["Goodbye!", "See you later!"],
+    "what is your name": ["My name is Rupak Parajuli"],
+    "which is the best programming language for basic":["Python is the best programming language for basic"]
+}
+
+# Define a function to handle the chatbot's response
+def chatbot_response(user_input):
+    # Check if the user's input is in the predefined responses
+    if user_input in responses:
+        # If so, return a random predefined response
+        return random.choice(responses[user_input])
+    else:
+        # If not, return a default response
+        return "I'm sorry, I don't understand what you're saying."
+
+# Start the chatbot
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "bye":
+        print(chatbot_response(user_input))
+        break
+    else:
+        print("Chatbot: ", chatbot_response(user_input))
