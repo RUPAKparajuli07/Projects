c263b89d5902b1819783a8c2ad4db891f2af3fd2
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-01-30 03:59:10 +0545

    Add files via upload

---
+++ typing test.py
@@ -0,0 +1,27 @@
+import time
+import random
+
+
+def typing_game():
+  words = [
+    "Google was founded on September 4, 1998, by Larry Page and Sergey Brin while they were PhD students at Stanford University in California. Together they own about 14% of its publicly listed shares and control 56% of its stockholder voting power through super-voting stock.",
+    "Its most common use so far is creating ChatGPT - a highly capable chatbot. To give you a little taste of its most basic ability, we asked GPT-3's chatbot to write its own description as you can see above. It’s a little bit boastful, but completely accurate and arguably very well written."
+  ]
+  word = random.choice(words)
+  print("Type the following word as fast as you can: " + word)
+  start = time.time()
+  user_input = input()
+  end = time.time()
+  time_elapsed = end - start
+  if user_input == word:
+    print("Correct! Time elapsed: " + str(time_elapsed) + " seconds")
+  else:
+    print("Incorrect. The word was: " + word)
+
+
+while True:
+  typing_game()
+  play_again = input("Do you want to play again? (yes/no)")
+  if play_again.lower() == 'no':
+    break
+print("Thank you for playing!")
