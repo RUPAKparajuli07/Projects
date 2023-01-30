b0042ff70c26df657d41fcac51e957be76e60d20
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-01-31 03:54:04 +0545

    Add files via upload

---
+++ emailvalidation check.py
@@ -0,0 +1,17 @@
+while True:
+    email = input("Enter email address: ")
+    at_index = email.find("@")
+    dot_index = email.rfind(".")
+
+    if email == "":
+        print("Email address not entered. Exiting loop.")
+        break
+    elif at_index > 0 and dot_index > at_index + 1 and dot_index < len(email) - 1:
+        print("Valid email.")
+        choice = input("Do you want to enter another email address (yes/no)? ")
+        if choice.lower() == "yes":
+            continue
+        else:
+            break
+    else:
+        print("Invalid email. Try again.")
