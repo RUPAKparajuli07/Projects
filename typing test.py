fd41e3217cfd72ad07e7ecb607fadab599aeec9f
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-02-03 11:45:42 +0545

    Update typing test.py

--- typing test.py
+++ typing test.py
@@ -5,8 +5,11 @@ import random
 def typing_game():
   words = [
     "Google was founded on September 4, 1998, by Larry Page and Sergey Brin while they were PhD students at Stanford University in California. Together they own about 14% of its publicly listed shares and control 56% of its stockholder voting power through super-voting stock.",
-    "Its most common use so far is creating ChatGPT - a highly capable chatbot. To give you a little taste of its most basic ability, we asked GPT-3's chatbot to write its own description as you can see above. It’s a little bit boastful, but completely accurate and arguably very well written."
+    "Its most common use so far is creating ChatGPT - a highly capable chatbot. To give you a little taste of its most basic ability, we asked GPT-3's chatbot to write its own description as you can see above. It’s a little bit boastful, but completely accurate and arguably very well written.",
+    "Facebook is a social networking platform that allows users to connect with friends, family, and communities. It was founded in 2004 and has since become one of the largest social media websites in the world with over 2.8 billion monthly active users. Users can share photos, videos, updates, and messages, as well as join groups, attend events, and create pages."
   ]
+  
+  
   word = random.choice(words)
   print("Type the following word as fast as you can: " + word)
   start = time.time()
