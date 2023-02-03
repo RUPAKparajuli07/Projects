895b52886ec6581d84cceefad563454bddf371c9
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-02-03 22:44:55 +0545

    Update simple chatbot.py

--- simple chatbot.py
+++ simple chatbot.py
@@ -1,6 +1,4 @@
 import random
-
-# Define a list of predefined responses
 responses = {
     "hi": ["Hello!", "Hi there!"],
     "how are you": ["I'm good, thanks for asking.", "I'm doing well, how about you?"],
@@ -10,17 +8,20 @@ responses = {
     "who are you":["i am ai"]
 }
 
-# Define a function to handle the chatbot's response
+
+#you can add more question if you want
+
+
 def chatbot_response(user_input):
-    # Check if the user's input is in the predefined responses
+   
     if user_input in responses:
-        # If so, return a random predefined response
+  
         return random.choice(responses[user_input])
     else:
-        # If not, return a default response
+        
         return "I'm sorry, I don't understand what you're saying."
 
-# Start the chatbot
+
 while True:
     user_input = input("You: ")
     if user_input.lower() == "bye":
