06028990acfa4404035ac8adcdafb0527a908338
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-02-25 14:47:35 +0545

    Update temperature detector.py
    Signed-off-by: RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>

--- temperature detector.py
+++ temperature detector.py
@@ -1,72 +1,57 @@
-import requests
 import tkinter as tk
-from tkinter import font
-import time
+import requests
 
-API_KEY = "bc295a4bc8727b69c6df749ba9cc88be"
+# Enter your OpenWeatherMap API key here
+API_KEY = 'bc295a4bc8727b69c6df749ba9cc88be'
 
-def get_temperature(api_key, city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+def get_weather(city):
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
     response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        temperature = data["main"]["temp"] - 273.15
-        return temperature
-    else:
-        return "Error: Could not retrieve temperature data."
+    weather_data = response.json()
+    temp = round(weather_data['main']['temp'] - 273.15, 2)
+    feels_like = round(weather_data['main']['feels_like'] - 273.15, 2)
+    weather = weather_data['weather'][0]['description']
+    return (temp, feels_like, weather)
 
-def update_temperature():
-    temperature = get_temperature(API_KEY, city_entry.get())
-    if isinstance(temperature, str):
-        temperature_label.config(text=temperature, fg="black")
+def update_weather():
+    city = city_entry.get()
+    if city:
+        try:
+            temp, feels_like, weather = get_weather(city)
+            temp_label.config(text=f'Temperature: {temp}°C', fg='white', bg='#434343')
+            feels_like_label.config(text=f'Feels Like: {feels_like}°C', fg='white', bg='#434343')
+            weather_label.config(text=f'Weather: {weather}', fg='white', bg='#434343')
+        except:
+            temp_label.config(text='Could not fetch weather data.', fg='white', bg='#434343')
+            feels_like_label.config(text='', bg='#434343')
+            weather_label.config(text='', bg='#434343')
     else:
-        temperature_label.config(text=f"{temperature:.2f}°C")
-        if temperature < 10:
-            temperature_label.config(fg="blue")
-        elif temperature < 20:
-            temperature_label.config(fg="green")
-        else:
-            temperature_label.config(fg="red")
+        temp_label.config(text='Please enter a city name.', fg='white', bg='#434343')
+        feels_like_label.config(text='', bg='#434343')
+        weather_label.config(text='', bg='#434343')
 
 # Create the main window
-window = tk.Tk()
-window.title("Weather App")
+root = tk.Tk()
+root.geometry('400x200')
+root.configure(bg='#434343')
+root.title('Weather App')
 
-# Set the background color based on the current time
-current_time = int(time.strftime("%H"))
-if current_time < 6 or current_time > 18:
-    window.configure(background="dark blue")
-else:
-    window.configure(background="light blue")
+# Create the input section
+input_frame = tk.Frame(root, bg='#434343')
+input_frame.pack(pady=10)
+city_entry = tk.Entry(input_frame, width=20, font=('Calibri', 16))
+city_entry.pack(side=tk.LEFT, padx=5)
+submit_button = tk.Button(input_frame, text='Submit', font=('Calibri', 14), command=update_weather)
+submit_button.pack(side=tk.LEFT)
 
-# Create a frame for the city entry and label
-city_frame = tk.Frame(window)
-city_frame.grid(row=0, column=0, padx=10, pady=10)
+# Create the output section
+output_frame = tk.Frame(root, bg='#434343')
+output_frame.pack()
+temp_label = tk.Label(output_frame, font=('Calibri', 20), bg='#434343')
+temp_label.pack(pady=5)
+feels_like_label = tk.Label(output_frame, font=('Calibri', 16), bg='#434343')
+feels_like_label.pack(pady=5)
+weather_label = tk.Label(output_frame, font=('Calibri', 16), bg='#434343')
+weather_label.pack(pady=5)
 
-# Create the city entry and label
-city_label = tk.Label(city_frame, text="Enter City:")
-city_label.grid(row=0, column=0)
-city_entry = tk.Entry(city_frame, width=20)
-city_entry.grid(row=0, column=1, padx=5)
-city_entry.focus()
-
-# Create a frame for the temperature label
-temperature_frame = tk.Frame(window)
-temperature_frame.grid(row=1, column=0, padx=10, pady=10)
-
-# Create the temperature label
-temperature_font = font.Font(size=24)
-temperature_label = tk.Label(temperature_frame, text="", font=temperature_font)
-temperature_label.pack()
-
-# Create the button to update the temperature
-update_button = tk.Button(window, text="Update Temperature", command=update_temperature)
-update_button.grid(row=2, column=0, padx=10, pady=10)
-
-# Make the window resizable
-window.rowconfigure(0, weight=1)
-window.rowconfigure(1, weight=1)
-window.columnconfigure(0, weight=1)
-
-# Start the main loop
-window.mainloop()
+root.mainloop()
