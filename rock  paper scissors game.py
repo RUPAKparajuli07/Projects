626e001a44965291c4713ac88e35045f5147393b
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-03-01 16:18:00 +0545

    Update rock  paper scissors game.py

--- rock  paper scissors game.py
+++ rock  paper scissors game.py
@@ -1,21 +1,46 @@
 import random
 
-choices = ["rock", "paper", "scissors"]
+# define the options
+options = ["rock", "paper", "scissors"]
 
-for i in range(3):
-    player_choice = input("Enter your choice (rock, paper, or scissors): ")
-    computer_choice = random.choice(choices)
-    print("Computer chooses: " + computer_choice)
+# define colors
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+END = '\033[0m'
 
-    if player_choice == computer_choice:
-        print("It's a tie!")
-    elif player_choice == "rock" and computer_choice == "scissors":
-        print("You win!")
-    elif player_choice == "paper" and computer_choice == "rock":
-        print("You win!")
-    elif player_choice == "scissors" and computer_choice == "paper":
-        print("You win!")
+# print game title and instructions
+print(f'{GREEN}{"="*35}\n{" "*10}Rock Paper Scissors\n{"="*35}{END}\n')
+print(f'{YELLOW}Welcome to the game!{END}\n{YELLOW}To play, choose rock, paper, or scissors.{END}')
+
+# get user input
+user_choice = input(f'\n{YELLOW}Your choice: {END}').lower()
+
+# generate computer choice
+computer_choice = random.choice(options)
+
+# print choices and design
+print(f'\n{YELLOW}You chose:{END}     {user_choice}')
+print(f'{YELLOW}Computer chose:{END} {computer_choice}\n')
+print(f'{GREEN}{"-"*35}{END}')
+
+# determine winner
+if user_choice == computer_choice:
+    print(f'{YELLOW}It\'s a tie!{END}')
+elif user_choice == "rock":
+    if computer_choice == "scissors":
+        print(f'{YELLOW}You win!{END}')
     else:
-        print("You lose!")
-
- 
+        print(f'{RED}Computer wins!{END}')
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print(f'{YELLOW}You win!{END}')
+    else:
+        print(f'{RED}Computer wins!{END}')
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print(f'{YELLOW}You win!{END}')
+    else:
+        print(f'{RED}Computer wins!{END}')
+else:
+    print(f'{RED}Invalid choice. Please choose rock, paper, or scissors.{END}')
