1dd61e18fb8a0f9ddf8c8321a2c2a8a68b986035
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-03-01 16:33:41 +0545

    Update rock  paper scissors game.py

--- rock  paper scissors game.py
+++ rock  paper scissors game.py
@@ -1,46 +1,81 @@
+import turtle
 import random
+import colorsys
 
 # define the options
 options = ["rock", "paper", "scissors"]
 
-# define colors
-GREEN = '\033[92m'
-YELLOW = '\033[93m'
-RED = '\033[91m'
-END = '\033[0m'
+# create a rainbow color scheme
+num_colors = 12
+colors = [colorsys.hsv_to_rgb(i/num_colors, 1.0, 1.0) for i in range(num_colors)]
 
-# print game title and instructions
-print(f'{GREEN}{"="*35}\n{" "*10}Rock Paper Scissors\n{"="*35}{END}\n')
-print(f'{YELLOW}Welcome to the game!{END}\n{YELLOW}To play, choose rock, paper, or scissors.{END}')
+# create the turtle screen and set the background color
+screen = turtle.Screen()
+screen.bgcolor('#f2f2f2')
+
+# create the game title and instructions
+title = turtle.Turtle()
+title.speed(0)
+title.penup()
+title.goto(0, 200)
+title.color('#333333')
+title.write('Rock Paper Scissors', align='center', font=('Arial', 30, 'bold'))
+instructions = turtle.Turtle()
+instructions.speed(0)
+instructions.penup()
+instructions.goto(0, 150)
+instructions.color('#333333')
+instructions.write('To play, choose rock, paper, or scissors.', align='center', font=('Arial', 16, 'normal'))
 
 # get user input
-user_choice = input(f'\n{YELLOW}Your choice: {END}').lower()
+user_choice = turtle.textinput('Rock Paper Scissors', 'Your choice:').lower()
 
 # generate computer choice
 computer_choice = random.choice(options)
 
-# print choices and design
-print(f'\n{YELLOW}You chose:{END}     {user_choice}')
-print(f'{YELLOW}Computer chose:{END} {computer_choice}\n')
-print(f'{GREEN}{"-"*35}{END}')
-
 # determine winner
 if user_choice == computer_choice:
-    print(f'{YELLOW}It\'s a tie!{END}')
+    winner = 'tie'
+    result_color = YELLOW
 elif user_choice == "rock":
     if computer_choice == "scissors":
-        print(f'{YELLOW}You win!{END}')
+        winner = 'you'
+        result_color = colors[0]
     else:
-        print(f'{RED}Computer wins!{END}')
+        winner = 'computer'
+        result_color = colors[6]
 elif user_choice == "paper":
     if computer_choice == "rock":
-        print(f'{YELLOW}You win!{END}')
+        winner = 'you'
+        result_color = colors[3]
     else:
-        print(f'{RED}Computer wins!{END}')
+        winner = 'computer'
+        result_color = colors[9]
 elif user_choice == "scissors":
     if computer_choice == "paper":
-        print(f'{YELLOW}You win!{END}')
+        winner = 'you'
+        result_color = colors[6]
     else:
-        print(f'{RED}Computer wins!{END}')
+        winner = 'computer'
+        result_color = colors[0]
 else:
-    print(f'{RED}Invalid choice. Please choose rock, paper, or scissors.{END}')
+    winner = 'invalid'
+    result_color = RED
+
+# create the game result
+result = turtle.Turtle()
+result.speed(0)
+result.penup()
+result.goto(0, -50)
+result.hideturtle()
+result.write('Result:', align='center', font=('Arial', 24, 'bold'))
+result.goto(0, -80)
+result.write(f'{winner.title()} chose {user_choice}', align='center', font=('Arial', 18, 'normal'))
+result.goto(0, -110)
+result.write(f'Computer chose {computer_choice}', align='center', font=('Arial', 18, 'normal'))
+result.goto(0, -150)
+result.write(f'{winner.title()} win!', align='center', font=('Arial', 24, 'bold'))
+result.color(result_color)
+
+# keep the turtle screen open until the user clicks to close it
+screen.mainloop()
