e9f0b8a3955b05f7bfe0a944a9f7ae7839108ad9
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-04-27 20:49:19 +0545

    Update race winner guess .py

--- race winner guess .py
+++ race winner guess .py
@@ -4,7 +4,6 @@ cars = ["BMW", "Audi", "Mercedes", "Porsche", "Ferrari"]
 
 # Asking user to pick the car
 user_car = input("Which car would you like to pick for the race (options: BMW, Audi, Mercedes, Porsche, Ferrari): ")
-
 #Checking if user input is valid
 if user_car not in cars:
     print("Invalid car selected. Please select a valid car.")
