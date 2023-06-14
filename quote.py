c36f07642c998a04e7f9133c2aa1522c583333c7
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-06-14 18:49:08 +0545

    Update quote.py

--- quote.py
+++ quote.py
@@ -2,6 +2,7 @@
 import random
 import os
 
+
 quotes = [
     "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
 "Believe you can and you're halfway there.",
