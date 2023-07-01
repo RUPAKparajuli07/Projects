7d35caa21d119d7cb054d1b21376a9be476ff57d
RUPAKparajuli07 <110843279+RUPAKparajuli07@users.noreply.github.com>
2023-07-01 12:06:27 +0545

    Update web browser.py

--- web browser.py
+++ web browser.py
@@ -3,8 +3,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
- 
-class MainWindow(QMainWindow):
+ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
